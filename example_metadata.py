193aab6d6b3eb6d2c7cf60213334d43c4ee44efa
Jono <ngsmicro@gmai.com>
2019-06-01 13:51:23 +0100

    Removed all global variables. End user variables are added to metadata.py. Version info added to class Biosys_Version.

---
+++ example_metadata.py
@@ -0,0 +1,15 @@
+#Metadata values, this allows me to share the script if needed.
+
+no_value = "no_value"
+
+community_analysis_to_keep_list = ["all"]
+
+control_regions = ["Blanks", "Positives", "Gblocks", "NTCs", "Unknowns", "TR"]
+
+batch_num_dict = {  "Run_1":"25-01-19",
+                    "Run_2":"15-01-19",
+                    "Run_3":"18-01-19",
+                    "Run_4":"21-01-19",
+                    "Run_5":"18-01-19"
+                    }
+
